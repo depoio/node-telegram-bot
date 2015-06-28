4dcfff82670a648d6dc2e171066417ba12a4eec2
longstone <git@longstone.ch>
2015-06-28 12:51:23 +0200

    echo bot for testing  / documentation purpose

---
+++ examples/echoBotExample.js
@@ -0,0 +1,45 @@
+/**
+ * Created by longstone on 28/06/15.
+ */
+'use strict';
+var Bot = require('./lib/Bot'), fs = require('fs');
+/**
+ * this sample helps understand how the bot works, can also be used for integration tests ;)
+ * @type {*|number}
+ */
+var bot = new Bot({
+    token: 'TOKEN HERE'
+})
+    .on('message', function (message) {
+        switch (message.text) {
+            case "/sendPhoto":
+                bot.sendPhoto({
+                    chat_id: message.chat.id,
+                    caption: 'trololo',
+                    files: {
+                        photo: {
+                            filename: 'logo.png',
+                            stream: fs.createReadStream('logo.png')
+                        }
+                    }
+                });
+                break;
+            case "/sendMessage":
+                bot.sendMessage({
+                    chat_id: message.chat.id,
+                    text: 'echo : ' + message.text
+                });
+                break;
+            case "/sendLocation":
+                bot.sendLocation({
+                    chat_id: message.chat.id,
+                    latitude: -27.121192,
+                    longitude: -109.366424,
+                    reply_to_message_id: message.message_id
+                })
+                ;
+                break;
+        }
+
+    })
+    .start();
