a8848df2e05712e55459ff4dd669b479e6aab95c
Shern Shiou Tan <shernshiou@gmail.com>
2015-06-29 10:16:32 +0800

    More test

--- test/index.js
+++ test/index.js
@@ -7,11 +7,23 @@ var TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11';
 
 describe('Telegram Bot client general test', function () {
   var bot;
-  it('should instantiate Telegram Bot client with correct token and values', function (done) {
+
+  before(function (done) {
     bot = new Bot({ token: TOKEN });
+    done();
+  });
+
+  it('should instantiate Telegram Bot client with correct token and values', function (done) {
     bot.base_url.should.equal('https://api.telegram.org/bot');
     bot.polling.should.equal(false);
     bot.token.should.equal(TOKEN);
+    done();
+  });
+
+  it('should have the essential functions', function (done) {
+    bot._get.should.exist;
+    bot._multipart.should.exist;
+    bot._poll.should.exist;
     done();
   });
 });
