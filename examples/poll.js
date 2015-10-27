50af69b12fa372862b25dda092269be80617da5c
Shern Shiou Tan <shernshiou@gmail.com>
2015-10-27 20:29:17 +0800

    Fix stop

--- examples/poll.js
+++ examples/poll.js
@@ -1,9 +1,19 @@
-var Bot = require('./lib/Bot');
+var Bot = require('../lib/Bot');
 
 var bot = new Bot({
-  token: 'TOKEN HERE'
+  token: 'TOKEN_HERE'
 })
 .on('message', function (message) {
+  console.log(message);
+})
+.on('stop', function (message) {
+  console.log('stop');
+  bot.stop();
+})
+.on('start', function (message) {
+  bot.start();
+})
+.on('error', function (message) {
   console.log(message);
 })
 .start();
