0d84512eea217f7c79e7a018fcf49e9f5965dd45
Shern Shiou Tan <shernshiou@gmail.com>
2016-01-06 13:31:20 +0800

    Basic test on class

--- test/Bot.test.js
+++ test/Bot.test.js
@@ -1,9 +1,21 @@
-var Bot = require('../index')
+var chai = require('chai')
+  , should = chai.should()
+  , Bot = require('../index');
+
+var TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11';
 
 describe('Telegram Bot client general test', function () {
-  it('should instantiate Telegram Bot client', function (done) {
-    var bot = new Bot();
-    console.log(bot);
+  var bot;
+
+  before(function (done) {
+    bot = new Bot({ token: TOKEN });
+    done();
+  });
+
+  it('should instantiate Telegram Bot client with correct token and values', function (done) {
+    bot.base_url.should.equal('https://api.telegram.org/');
+    bot.polling.should.equal(false);
+    bot.token.should.equal(TOKEN);
     done();
   });
 });
